cea8ec091e1e3e6dbd3da8b3b8f785af893ae698
Gammasoft <gammasoft71@gmail.com>
2025-07-22 12:14:28 +0200

    Update target_properties.cmake

--- password_generator/properties/target_properties.cmake
+++ password_generator/properties/target_properties.cmake
@@ -1,4 +1,4 @@
 target_default_namespace("password_generator")
 target_icon(resources/password_generator)
 target_name("password_generator")
-target_startup("password_generator::password_generator_form" src/password_generator_form.h)
+target_startup("password_generator::password_generator_form" "src/password_generator_form.hpp")
